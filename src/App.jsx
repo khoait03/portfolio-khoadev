dffadbaf61ae149027ea5c07f66b386b4b82f1b0
khoait03 <132044212+khoait03@users.noreply.github.com>
2025-03-06 19:44:41 +0700

    Fix không click vào menu mobile được

--- src/App.jsx
+++ src/App.jsx
@@ -33,51 +33,51 @@ const App = () => {
                  <Experience />
                  <Project />
                  <Contact />
-                 
+
                  <div className="fixed bottom-5 right-5 md:bottom-auto md:top-1/2 md:-translate-y-1/2 bg-white text-neutral-900 dark:bg-neutral-800 dark:text-neutral-200 flex flex-row md:flex-col gap-2 items-center rounded-full px-3 py-2 shadow-lg">
           
                       {/* <div className="fixed bottom-10 right-5 bg-white text-neutral-900 dark:bg-neutral-800 dark:text-neutral-200 flex flex-col gap-2 items-center rounded-full py-1 shadow-lg"> */}
                       <button className="relative group p-2 cursor-pointer" onClick={toggleLanguage}>
                            {language === 'vietnamese' ? 'EN' : 'VI'}
-                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap ">
+                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap hidden md:block ">
                                 <span>{language === 'vietnamese' ? 'Ngôn ngữ' : 'Language'}</span>
                            </span>
                       </button>
                       <button className="relative group p-2 cursor-pointer" onClick={toggleDarkMode}>
                            {darkMode ? <CiLight className="text-2xl" /> : <CiDark className="text-2xl" />}
-                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap ">
+                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap hidden md:block ">
                                 <span>{language === 'vietnamese' ? 'Sáng - Tối' : 'Dark Mode'}</span>
                            </span>
                       </button>
                       <a href="#about" className="relative group p-2 cursor-pointer">
                            <CiUser className="text-2xl" />
-                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap ">
+                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap hidden md:block ">
                                 <span>{language === 'vietnamese' ? 'Giới thiệu' : 'About me'}</span>
                            </span>
                       </a>
                       <a href="#skill" className="relative group p-2 cursor-pointer">
                            <CiStar className="text-2xl" />
-                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap ">
+                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap hidden md:block ">
                                 <span>{language === 'vietnamese' ? 'Kỹ năng' : 'Skill'}</span>
                            </span>
                       </a>
                       <a href="#experiences" className="relative group p-2 cursor-pointer">
                            <CiAlarmOn className="text-2xl" />
-                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap">
+                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap hidden md:block">
                                 {language === 'vietnamese' ? 'Hành trình' : 'Timeline'}
                            </span>
                       </a>
 
                       <a href="#projects" className="relative group p-2 cursor-pointer">
                            <CiFolderOn className="text-2xl" />
-                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap">
+                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap hidden md:block">
                                 {language === 'vietnamese' ? 'Dự án' : 'Project'}
                            </span>
                       </a>
 
                       <a href="#contact" className="relative group p-2 cursor-pointer">
                            <CiMail className="text-2xl" />
-                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap">
+                           <span className="absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white text-lg px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity w-auto min-w-max whitespace-nowrap hidden md:block">
                                 {language === 'vietnamese' ? 'Liên hệ' : 'Contact'}
                            </span>
                       </a>
